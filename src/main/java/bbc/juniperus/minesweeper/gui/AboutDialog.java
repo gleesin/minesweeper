9f125eae20f7130ecb354d32a5182237bfc92f02
steppenratte <steppenratte@gmail.com>
2014-07-03 22:52:12 +0200

    testing sound in lin

--- src/main/java/bbc/juniperus/minesweeper/gui/AboutDialog.java
+++ src/main/java/bbc/juniperus/minesweeper/gui/AboutDialog.java
@@ -1,16 +1,26 @@
 package bbc.juniperus.minesweeper.gui;
 
 import java.awt.BorderLayout;
-
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import bbc.juniperus.minesweeper.gui.ResourceLoader.SoundResource;
 
 /**
  * A modal dialog which shows the information about the application and the
@@ -59,5 +69,22 @@ public class AboutDialog extends JDialog {
 
 		add(panel);
 		add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
+	public static void main(String[] args)
+			throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
+		System.out.println("Starting");
+		URL url = ResourceLoader.getInstance().getSoundResourceUrl(
+				SoundResource.WIN);
+		AudioInputStream audioStream = AudioSystem.getAudioInputStream(url);
+
+		AudioFormat format = audioStream.getFormat();
+		DataLine.Info info = new DataLine.Info(Clip.class, format);
+		Clip clip = (Clip) AudioSystem.getLine(info);
+		clip.open(audioStream);
+		clip.start();
+		
+		Thread.sleep(4000);
+		System.out.println("finished");
 	}
 }
