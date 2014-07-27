f143e6ac4f613bb3ab794bd4c1008651563e2341
Gleesin <seriouz.accz@gmail.com>
2014-07-27 21:25:24 +0200

    Renamed packages

--- src/main/java/sk/lkce/minesweeper/gui/AboutDialog.java
+++ src/main/java/sk/lkce/minesweeper/gui/AboutDialog.java
@@ -5,22 +5,11 @@ import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.URL;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-import sk.lkce.minesweeper.gui.ResourceLoader.SoundResource;
 
 /**
  * A modal dialog which shows the information about the application and the
@@ -29,7 +18,7 @@ import sk.lkce.minesweeper.gui.ResourceLoader.SoundResource;
 @SuppressWarnings("serial")
 public class AboutDialog extends JDialog {
 
-	private final static int HEIGHT = 120;
+	private final static int HEIGHT = 160;
 	private final static int WIDTH = 250;
 
 	public AboutDialog(Frame frame) {
@@ -48,7 +37,8 @@ public class AboutDialog extends JDialog {
 		JLabel label = new JLabel();
 		label.setText("<html>This is a Java Swing <br>"
 				+ "implementation of Microsoft's <br>classic Windows XP Minesweeper."
-				+ "<br<i>Version 1.0<i/>" + "</html>");
+				+ "<br<i>Version 1.0<i/>" +
+				"</html>");
 		JButton button = new JButton("Ok");
 		button.setPreferredSize(new Dimension(50, 20));
 		button.addActionListener(new ActionListener() {
@@ -71,20 +61,4 @@ public class AboutDialog extends JDialog {
 		add(buttonPanel, BorderLayout.SOUTH);
 	}
 	
-	public static void main(String[] args)
-			throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
-		System.out.println("Starting");
-		URL url = ResourceLoader.getInstance().getSoundResourceUrl(
-				SoundResource.WIN);
-		AudioInputStream audioStream = AudioSystem.getAudioInputStream(url);
-
-		AudioFormat format = audioStream.getFormat();
-		DataLine.Info info = new DataLine.Info(Clip.class, format);
-		Clip clip = (Clip) AudioSystem.getLine(info);
-		clip.open(audioStream);
-		clip.start();
-		
-		Thread.sleep(4000);
-		System.out.println("finished");
-	}
 }
